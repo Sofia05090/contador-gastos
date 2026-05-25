d08ec6561bd4776dfbea528f50eccae18d0020a1
Laura V Cardona <58284758+laura3212401957@users.noreply.github.com>
2026-05-25 02:14:29 +0000

    Cambios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SWRegistration from "./SWRegistration";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+export const metadata: Metadata = {
+  title: "contador gastos",
+  description: "generado para visualizar de una manera practica el contador de gastos",
+  manifest: "/manifest.json",
+};
 
 export default function RootLayout({
   children,
@@ -13,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className={inter.variable}>
+      <SWRegistration/>
       <body>{children}</body>
     </html>
   );
